c6db765b23aef23da30743981aff175b0eba583e
leah karienye <karienyeleah@gmail.com>
2022-06-22 04:18:46 +0300

    api integration

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-document.getElementsByClassName(grid).addEventListener(onclick)
+//document.addEventListener("DOMcontentloaded", () => {
+ // const eventsUrl ="https://rapidapi.com/stefan.skliarov/api/LondonTheatreDirect/"
 
-//document.getElementsByTagName(img) = src =""
+  
+//let myHeaders = new Headers();
+//myHeaders.append("Cookie", "i18n_redirected=en-US");
+
+function getEvents(){ 
+let requestOptions = {
+  method: 'GET',
+ // headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch(eventsUrl, requestOptions)
+  .then((response) => {
+    console.log(response())
+  //.then(result => console.log(result))
+  //.catch(error => console.log('error', error));
+  })
+ getEvents()
+};
+
 
 function openEvent(eventName, element, color) {
     let i, tabcontent, tablinks;
@@ -15,4 +35,17 @@ function openEvent(eventName, element, color) {
     document.getElementById(eventName).style.display = "block";
     element.style.backgroundColor = color;
 }
-    document.getElementById("defaultOpen").click();
+   // document.getElementById("defaultOpen").click();
+
+    function formSubmit(response){
+      getElementById(form).addEventListener( "display",response)
+
+
+    }
+    function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+    
+    function closeForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
