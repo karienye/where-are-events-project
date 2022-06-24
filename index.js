4f00694c1192c28d6781748cd2f5610428d5f125
leah karienye <karienyeleah@gmail.com>
2022-06-24 13:59:13 +0300

    display events js

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const projectUrl = "http://localhost:3000/events"
-  //const eventsUrl = "https://eventfinda-eventfinda-nz.p.rapidapi.com/events.json/?rapidapi-key=59db1ebdd8msh4851772c7df0242p11eba8jsn1072ca5a255b"
-  
- // "https://canada-holidays.p.rapidapi.com/api/v1/holidays/2/?rapidapi-key=59db1ebdd8msh4851772c7df0242p11eba8jsn1072ca5a255b"
+fetch("http://localhost:3000/events")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = '<b>NAME OF EVENT:</b> ' + data[i].name+'<br>' +'<b>EVENT:</b> ' + '"<img src="'+data[i].name+'" />'+'<br>'+'<b>DESCRIPTION:</b> ' + data[i].description + '<br> <br> <br>';
+                mainContainer.appendChild(div);
+               
+                
+            }
+        }
 
-  //
+        /*
+        for (var i=0; i< data["item"].images.length; i++) {
+var img = data["item"]images[i];
+}
 
-  let projectContainer = document.querySelector('#projectContainer')
+var alt = data["item"].name;
+    mhtml += '<li><div class=""><img src="'+val.img+'" /></div>';
+    mhtml += '<h1 class="title">'+alt+'</h1>';
+    mhtml += '</li>';
+});
+var $ul = $('<ul>').append($(mhtml));.
+$('#mydiv').append($ul);
 
-  function getEvents(){
-    let reqOptions = {
-      method: 'GET',
-    };
+});
+        */
 
-    fetch(projectUrl, reqOptions)
-      .then((response) => response.json())
-      .then(results => {
 
-        results.forEach(proItem => {
-          console.log(proItem)
-        })
-        results.forEach(proItem => {
-          events(proItem)
-        })
-      })
-
-  };
-  function events(itemPro) {
-    const proCard = `
-  <!-- <div class="col-4">
-                  <div class="proCont">
-                    <img src="${itemPro.url}"> </img>
-                    <h3>${itemPro.name}</h3>
-                    <p>"${itemPro.description}"</a>
-                    <p>"${itemPro.url_slug}"</a>
-                              
-                    </div>
-                  </div>
-                </div> -->
-  `
-    projectContainer.innerHTML += proCard
-  }
-  getEvents()
-})
 
 
 
